ea490424e159a9f54bd93282b2b37cb20eade8d0
MirkoValle <mirko.valle20@gmail.com>
2024-05-14 11:35:23 +0200

    Added snack 8, setted colors in css and called div inside the js script

---
+++ snack-8/js/script.js
@@ -0,0 +1,7 @@
+//Snack8 
+//Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
+
+const pari = document.getElementById("pari")
+const dispari = document.getElementById("dispari")
+
+
