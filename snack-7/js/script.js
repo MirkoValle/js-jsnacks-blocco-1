e26cba9418acc86bead9b6241c05f0143f48c0ae
MirkoValle <mirko.valle20@gmail.com>
2024-05-14 11:14:25 +0200

    Edited snack 7, deleted "10" on push function

--- snack-7/js/script.js
+++ snack-7/js/script.js
@@ -1,7 +1,7 @@
 //Snack7
 //Fai inserire un numero, che chiameremo N, all'utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
 
-const n = Number.parseInt(prompt("Inserisci un numero"), 10)
+const n = Number.parseInt(prompt("Inserisci un numero"), 10);
 
 for (let i = 0; i < n; i++) {
 
@@ -9,7 +9,7 @@ for (let i = 0; i < n; i++) {
 
     for (let x = 0; x < 10; x++) {
 
-        array.push(Math.floor(Math.random() * 100 + 1), 10)
+        array.push(Math.floor(Math.random() * 100 + 1));
 
     }
 
